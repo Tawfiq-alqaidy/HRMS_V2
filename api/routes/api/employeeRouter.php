7d2b31012a021ddc5db9075a6033c729cb4d1828
Tawfiq alqaidy <talqaidy@gmail.com>
2025-07-01 23:28:42 +0200

    fix: Correct order of routes in EmployeeController for clarity

--- api/routes/api/employeeRouter.php
+++ api/routes/api/employeeRouter.php
@@ -5,11 +5,11 @@ use App\Http\Controllers\EmployeeController;
 
 Route::prefix('employees')->group(function () {
     Route::get('/all', [EmployeeController::class, 'index'])->name('employees.index');
-    Route::post('/create', [EmployeeController::class, 'store'])->name('employees.store');
+    Route::get('/archived', [EmployeeController::class, 'archivedEmployees'])->name('employees.archivedEmployees');
     Route::get('/{id}', [EmployeeController::class, 'show'])->name('employees.show');
+    Route::post('/create', [EmployeeController::class, 'store'])->name('employees.store');
     Route::patch('/{id}', [EmployeeController::class, 'update'])->name('employees.update');
     Route::patch('/archiving/{id}', [EmployeeController::class, 'archiving'])->name('employees.archiving');
     Route::patch('/restoring/{id}', [EmployeeController::class, 'restore'])->name('employees.restore');
-    Route::get('/archived', [EmployeeController::class, 'archivedEmployees'])->name('employees.archivedEmployees');
     Route::delete('/{id}', [EmployeeController::class, 'destroy'])->name('employees.destroy');
 });

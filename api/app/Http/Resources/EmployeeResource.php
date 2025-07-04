84608e6e4c18142ce45cd02ca32c323e90c2995e
Tawfiq alqaidy <talqaidy@gmail.com>
2025-07-04 15:29:40 +0200

    refactor: Simplify EmployeeResource by removing unused fields from the toArray method

--- api/app/Http/Resources/EmployeeResource.php
+++ api/app/Http/Resources/EmployeeResource.php
@@ -16,21 +16,10 @@ class EmployeeResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'user_id' => $this->user_id,
             'department_name' => $this->department ? $this->department->name : "whithout department",
             'full_name' => $this->full_name,
-            'birth_date' => $this->birth_date,
-            'gender' => $this->gender,
             'phone' => $this->phone,
-            'picture' => $this->picture,
-            'basic_salary' => $this->basic_salary,
-            'bank_name' => $this->bank_name,
-            'bank_branch' => $this->bank_branch,
-            'bank_account_number' => $this->bank_account_number,
-            'resume_file' => $this->resume_file,
-            'isActive' => $this->isActive,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            'status' => $this->isActive ? 'active' : 'inactive',
         ];
     }
 }

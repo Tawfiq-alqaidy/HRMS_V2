7997836210df95dd6f161f14e102900f259be6b6
Tawfiq alqaidy <talqaidy@gmail.com>
2025-07-04 20:32:52 +0200

    fix: Change update route method from PATCH to POST for employee updates

--- api/routes/api/employeeRouter.php
+++ api/routes/api/employeeRouter.php
@@ -7,7 +7,7 @@ Route::prefix('employees')->group(function () {
     Route::get('/', [EmployeeController::class, 'index'])->name('employees.index');
     Route::get('/{id}', [EmployeeController::class, 'show'])->name('employees.show');
     Route::post('/create', [EmployeeController::class, 'store'])->name('employees.store');
-    Route::patch('/{id}', [EmployeeController::class, 'update'])->name('employees.update');
+    Route::post('/{id}', [EmployeeController::class, 'update'])->name('employees.update');
     Route::patch('/archiving/{id}', [EmployeeController::class, 'archiving'])->name('employees.archiving');
     Route::patch('/restoring/{id}', [EmployeeController::class, 'restore'])->name('employees.restore');
     Route::delete('/{id}', [EmployeeController::class, 'destroy'])->name('employees.destroy');
